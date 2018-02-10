63476d8df47a7aa87469fc800eb5409763fd4b32
vasanth <35736389+mappillaivasanth@users.noreply.github.com>
2018-02-10 12:05:17 +0530

    Update pointerandaddress.c

--- pointerandaddress.c
+++ pointerandaddress.c
@@ -4,18 +4,19 @@ int main()
 {
     int array[3],*p;
     p=array;
-    for(int i=1;i<=3;i++)
+    printf("Enter the values for array\n");
+    for(int i=0;i<3;i++)
     {
         scanf("%d\n",&array[i]);
     }
-    printf("The elements in the array is\n");
-    for(int i=1;i<=3;i++)
+    printf("The elements in the array are\n");
+    for(int i=0;i<=3;i++)
     {
         printf("%d\n",*p);
         p++;
     }
-    printf("The address in the array is\n");
-    for(int i=1;i<=3;i++)
+    printf("The address in the array are\n");
+    for(int i=0;i<3;i++)
     {
         printf("%d\n",p);
         p++;
@@ -23,3 +24,4 @@ int main()
 
     return 0;
 }
+
